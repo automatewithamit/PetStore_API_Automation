ec215b0e1f92097c93a002c6aeadb37cc465376b
amittripathi <automatewithamit@gmail.com>
2024-07-06 21:29:09 +0530

    Routes.java

---
+++ src/test/java/api/petstore/endpoints/Routes.java
@@ -0,0 +1,11 @@
+package api.petstore.endpoints;
+
+public class Routes {
+
+    public static String base_url = "https://petstore.swagger.io/v2";
+    public static String post_url = base_url +"/user";
+    public static String get_url = base_url +"/user/{username}";
+    public static String put_url = base_url +"/user/{username}";
+    public static String del_url = base_url +"/user/{username}";
+
+}
